1b2ce5a02260ce38d5248e5a0087531e2d6327ef
chenaski <mail2chenaski@gmail.com>
2022-03-28 23:12:00 +0300

    Implement `Linked List`

---
+++ data-structures/linked-list.js
@@ -0,0 +1,86 @@
+export class LinkedList {
+  #head;
+  #tail;
+
+  pop() {
+    return this.#tail?.value;
+  }
+
+  push(value) {
+    if (!this.#tail) {
+      this.#head = this.#tail = { value };
+    } else {
+      this.#tail.next = { value };
+      this.#tail = this.#tail.next;
+    }
+  }
+
+  shift() {
+    return this.#head?.value;
+  }
+
+  unshift(value) {
+    if (!this.#head) {
+      this.#head = this.#tail = { value };
+    } else {
+      this.#head = { value, next: this.#head };
+    }
+  }
+
+  forEach(cb) {
+    let prevNode = null;
+    let currentNode = this.#head;
+
+    while (currentNode) {
+      const stop = cb({ currentNode, prevNode });
+      if (stop) break;
+      prevNode = currentNode;
+      currentNode = currentNode.next;
+    }
+  }
+
+  contains(value) {
+    let found = false;
+
+    this.forEach(({ currentNode }) => {
+      if (value === currentNode.value) {
+        found = true;
+        return true;
+      }
+    });
+
+    return found;
+  }
+
+  remove(value) {
+    this.forEach(({ currentNode, prevNode }) => {
+      if (currentNode.value === value) {
+        if (currentNode.value === this.#head.value) {
+          this.#head = this.#head.next;
+        } else if (currentNode.value === this.#tail.value) {
+          delete prevNode.next;
+          this.#tail = prevNode;
+        } else if (prevNode) {
+          prevNode.next = currentNode.next;
+        }
+        return true;
+      }
+    });
+  }
+
+  reverse() {
+    let newTail = this.#head;
+    let newHead = null;
+
+    this.forEach(({ currentNode }) => {
+      if (!newHead) {
+        newHead = currentNode;
+      } else {
+        newHead = { ...currentNode, next: newHead };
+      }
+    });
+
+    this.#head = newHead;
+    this.#tail = newTail;
+  }
+}
---
+++ data-structures/linked-list.test.js
@@ -0,0 +1,153 @@
+import { LinkedList } from "./linked-list.js";
+
+describe("LinkedList", () => {
+  test("push", () => {
+    const linkedList = new LinkedList();
+
+    const expectedValue1 = Symbol("1");
+    const expectedValue2 = Symbol("2");
+    const expectedValue3 = Symbol("3");
+
+    linkedList.push(expectedValue1);
+
+    expect(linkedList.contains(expectedValue1)).toBe(true);
+    expect(linkedList.pop()).toBe(expectedValue1);
+    expect(linkedList.shift()).toBe(expectedValue1);
+
+    linkedList.push(expectedValue2);
+
+    expect(linkedList.contains(expectedValue2)).toBe(true);
+    expect(linkedList.pop()).toBe(expectedValue2);
+    expect(linkedList.shift()).toBe(expectedValue1);
+
+    linkedList.push(expectedValue3);
+
+    expect(linkedList.contains(expectedValue1)).toBe(true);
+    expect(linkedList.contains(expectedValue2)).toBe(true);
+    expect(linkedList.contains(expectedValue3)).toBe(true);
+    expect(linkedList.pop()).toBe(expectedValue3);
+    expect(linkedList.shift()).toBe(expectedValue1);
+  });
+
+  test("unshift", () => {
+    const linkedList = new LinkedList();
+
+    const expectedValue1 = Symbol("1");
+    const expectedValue2 = Symbol("2");
+    const expectedValue3 = Symbol("3");
+
+    linkedList.unshift(expectedValue1);
+
+    expect(linkedList.contains(expectedValue1)).toBe(true);
+    expect(linkedList.pop()).toBe(expectedValue1);
+    expect(linkedList.shift()).toBe(expectedValue1);
+
+    linkedList.unshift(expectedValue2);
+
+    expect(linkedList.contains(expectedValue2)).toBe(true);
+    expect(linkedList.pop()).toBe(expectedValue1);
+    expect(linkedList.shift()).toBe(expectedValue2);
+
+    linkedList.unshift(expectedValue3);
+
+    expect(linkedList.contains(expectedValue1)).toBe(true);
+    expect(linkedList.contains(expectedValue2)).toBe(true);
+    expect(linkedList.contains(expectedValue3)).toBe(true);
+    expect(linkedList.pop()).toBe(expectedValue1);
+    expect(linkedList.shift()).toBe(expectedValue3);
+  });
+
+  test("remove", () => {
+    const linkedList = new LinkedList();
+
+    const expectedValue1 = Symbol("1");
+    const expectedValue2 = Symbol("2");
+    const expectedValue3 = Symbol("3");
+    const expectedValue4 = Symbol("4");
+    const expectedValue5 = Symbol("5");
+
+    linkedList.push(expectedValue1);
+    linkedList.push(expectedValue2);
+    linkedList.push(expectedValue3);
+    linkedList.push(expectedValue4);
+    linkedList.push(expectedValue5);
+
+    expect(linkedList.contains(expectedValue1)).toBe(true);
+    expect(linkedList.contains(expectedValue2)).toBe(true);
+    expect(linkedList.contains(expectedValue3)).toBe(true);
+    expect(linkedList.contains(expectedValue4)).toBe(true);
+    expect(linkedList.contains(expectedValue5)).toBe(true);
+
+    expect(linkedList.pop()).toBe(expectedValue5);
+    expect(linkedList.shift()).toBe(expectedValue1);
+
+    linkedList.remove(expectedValue1);
+
+    expect(linkedList.contains(expectedValue1)).toBe(false);
+    expect(linkedList.contains(expectedValue2)).toBe(true);
+    expect(linkedList.contains(expectedValue3)).toBe(true);
+    expect(linkedList.contains(expectedValue4)).toBe(true);
+    expect(linkedList.contains(expectedValue5)).toBe(true);
+
+    expect(linkedList.pop()).toBe(expectedValue5);
+    expect(linkedList.shift()).toBe(expectedValue2);
+
+    linkedList.remove(expectedValue5);
+
+    expect(linkedList.contains(expectedValue1)).toBe(false);
+    expect(linkedList.contains(expectedValue2)).toBe(true);
+    expect(linkedList.contains(expectedValue3)).toBe(true);
+    expect(linkedList.contains(expectedValue4)).toBe(true);
+    expect(linkedList.contains(expectedValue5)).toBe(false);
+
+    expect(linkedList.pop()).toBe(expectedValue4);
+    expect(linkedList.shift()).toBe(expectedValue2);
+
+    linkedList.remove(expectedValue3);
+
+    expect(linkedList.contains(expectedValue1)).toBe(false);
+    expect(linkedList.contains(expectedValue2)).toBe(true);
+    expect(linkedList.contains(expectedValue3)).toBe(false);
+    expect(linkedList.contains(expectedValue4)).toBe(true);
+    expect(linkedList.contains(expectedValue5)).toBe(false);
+
+    expect(linkedList.pop()).toBe(expectedValue4);
+    expect(linkedList.shift()).toBe(expectedValue2);
+  });
+
+  test("reverse", () => {
+    const linkedList = new LinkedList();
+
+    const expectedValue1 = Symbol("1");
+    const expectedValue2 = Symbol("2");
+    const expectedValue3 = Symbol("3");
+    const expectedValue4 = Symbol("4");
+    const expectedValue5 = Symbol("5");
+
+    linkedList.push(expectedValue1);
+    linkedList.push(expectedValue2);
+    linkedList.push(expectedValue3);
+    linkedList.push(expectedValue4);
+    linkedList.push(expectedValue5);
+
+    expect(linkedList.contains(expectedValue1)).toBe(true);
+    expect(linkedList.contains(expectedValue2)).toBe(true);
+    expect(linkedList.contains(expectedValue3)).toBe(true);
+    expect(linkedList.contains(expectedValue4)).toBe(true);
+    expect(linkedList.contains(expectedValue5)).toBe(true);
+
+    expect(linkedList.pop()).toBe(expectedValue5);
+    expect(linkedList.shift()).toBe(expectedValue1);
+
+    linkedList.reverse();
+
+    expect(linkedList.contains(expectedValue1)).toBe(true);
+    expect(linkedList.contains(expectedValue2)).toBe(true);
+    expect(linkedList.contains(expectedValue3)).toBe(true);
+    expect(linkedList.contains(expectedValue4)).toBe(true);
+    expect(linkedList.contains(expectedValue5)).toBe(true);
+
+    expect(linkedList.pop()).toBe(expectedValue1);
+    expect(linkedList.shift()).toBe(expectedValue5);
+  });
+});
